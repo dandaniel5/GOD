961d01ad5f5a47432d0a3012f2b91e6726e58cd6
[Shugaf433] <tg@Shugaf433>
2024-01-27 03:41:13 +0400

    add next and shadcn

---
+++ next/app/page.js
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  );
+}
